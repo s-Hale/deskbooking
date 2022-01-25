958e630772cbee0de1a156d3638bfb1d37752ec3
Sally Hale <sally.hale@brighthr.com>
2022-01-25 23:32:57 +0000

    really just like cyan

--- src/components/FirstFolder/index.js
+++ src/components/FirstFolder/index.js
@@ -6,10 +6,12 @@ import styled from 'styled-components';
 
 
 const DeskMap = () => {
-    const [numberHeight, setNumberHeight] = useState(5);
+    const [numberHeight, setNumberHeight] = useState(24);
     const [numberColumns, setNumberColumns] = useState(8);
     const [groupGapOne, setGroupGapOne] = useState(2);
-    const [groupGapTwo, setGroupGapTwo] = useState(6)
+    const [groupGapTwo, setGroupGapTwo] = useState(6);
+    const [chosenDesk, setChosenDesk] = useState();
+    const [summaryPhrase, setSummaryPhrase] = useState('');
 
     const StyledDesks = styled.div`
      & > span:nth-of-type(${numberColumns}n + ${groupGapOne}) {
@@ -18,29 +20,47 @@ const DeskMap = () => {
      & > span:nth-of-type(${numberColumns}n + ${groupGapTwo}) {
         margin-right: 24px
      }
+     grid-template-columns: repeat(${numberColumns}, min-content);
     `;
 
+    const handleDeskSelection = (e) => {
+        setChosenDesk(e.target.value)
+        setSummaryPhrase(`You have selected desk ${e.target.value}.`)
+    }
 
 
     const desks = Array.from({ length: numberHeight }, (_, i) => i)
     return (
         <>
-            <StyledDesks style={{'grid-template-columns': 'repeat(' + numberColumns + ', min-content)'}} className="mt-16 desks bg-white grid rounded-md p-2">
+            <StyledDesks className="mt-16 desks grid rounded-md p-2">
             {desks.map(seat =>  (
               <span
                 tabIndex="0"
                 key={seat}
-                className="m-1 desk w-24 h-12 border border-gray-600 rounded-md hover:bg-blue-500 cursor-pointer text-xs text-gray-800 flex items-center justify-center"
-              >{seat}</span>
+                className={Number(chosenDesk) === seat ? 'bg-cyan-600 m-1 rounded-md' : 'bg-blue-50 m-1 rounded-md'}
+                >
+                    <button
+                        value={seat}
+                        onClick={(e) => handleDeskSelection(e)}
+                        className="w-24 h-12 border border-cyan-900 rounded-md hover:bg-cyan-600 cursor-pointer text-xs text-gray-800 flex items-center justify-center">{seat}</button>
+                </span>
              )
-           )}
+                )}
+
+
+
             </StyledDesks>
             <UserInputForm
+                numberHeight={numberHeight}
+                numberColumns={numberColumns}
+                groupGapOne={groupGapOne}
+                groupGapTwo={groupGapTwo}
                 setNumberHeight={setNumberHeight}
                 setNumberColumns={setNumberColumns}
                 setGroupGapOne={setGroupGapOne}
                 setGroupGapTwo={setGroupGapTwo}
             />
+            <span className="mt-2 text-lg">{summaryPhrase}</span>
         </>
     )
 }
